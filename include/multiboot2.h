b3a3a48b5332b8673fdeff06673a4be1466b6596
Seongjae Kim <kimseongjae0514@gmail.com>
2026-09-18 22:42:16 +0900

    feat: Implement Multiboot2 information parser

--- include/multiboot2.h
+++ include/multiboot2.h
@@ -1,11 +1,40 @@
 #ifndef MULTIBOOT2_H
 #define MULTIBOOT2_H
 
-#ifndef __ASSEMBLER__
-#include <fxos/types.h>
-#endif
-
 #define MULTIBOOT2_HEADER_MAGIC 0xE85250D6
 #define MULTIBOOT2_ARCH_I386    0
+
+#define MULTIBOOT2_TAG_MMAP     6
+
+#ifndef __ASSEMBLER__
+
+#include <fxos/types.h>
+
+struct multiboot2_tag {
+    uint32_t type;
+    uint32_t size;
+};
+
+struct multiboot2_info {
+    uint32_t total_size;
+    uint32_t reserved;
+    struct multiboot2_tag tags[];
+};
+
+struct multiboot2_mmap_entry {
+    uint64_t addr;
+    uint64_t len;
+    uint32_t type;
+    uint32_t reserved;
+};
+
+struct multiboot2_mmap {
+    struct multiboot2_tag head;
+    uint32_t entry_size;
+    uint32_t entry_version;
+    struct multiboot2_mmap_entry entries[];
+};
+
+#endif
 
 #endif
--- init/main.c
+++ init/main.c
@@ -5,6 +5,33 @@
 #include <fxos/rtl.h>
 #include <multiboot2.h>
 
+char *multiboot2_tag_names[] = {
+    NULL,
+    "Boot command line",
+    "Bootloader name",
+    "Modules",
+    "Basic memory information",
+    "BIOS boot device",
+    "Memory map",
+    "VBE",
+    "Framebuffer",
+    "ELF sections",
+    "APM",
+    "EFI 32-bit system table",
+    "EFI 64-bit system table",
+    "SMBIOS information",
+    "ACPI RSDP 1.0",
+    "ACPI RSDP 2.0",
+    "Network configurations",
+    "EFI memory map",
+    "EFI boot services",
+    "EFI 32-bit image handle",
+    "EFI 64-bit image handle",
+    "Load base address"
+};
+
+struct multiboot2_mmap *mmap;
+
 static void printf(const char *fmt, ...)
 {
     va_list args;
@@ -13,7 +40,20 @@ static void printf(const char *fmt, ...)
     va_end(args);
 }
 
-void kmain(uint32_t magic, void *info)
+static void parse_loader_info(struct multiboot2_info *info)
+{
+    struct multiboot2_tag *tag = info->tags;
+
+    printf("multiboot2: GRUB-provided informations\n");
+
+    while (tag->type != 0)
+    {
+        printf("multiboot2-tag: type=%u <%s> size=%u\n", tag->type, multiboot2_tag_names[tag->type], tag->size);
+        tag = (struct multiboot2_tag*)ALIGN_UP((uintptr_t)tag + tag->size, 8);
+    }
+}
+
+void kmain(uint32_t magic, struct multiboot2_info *info)
 {
     (void)magic;(void)info;
     
@@ -22,6 +62,8 @@ void kmain(uint32_t magic, void *info)
     init_interrupt();
 
     printf("Hello, kernel!\nmagic=0x%X info=%p\n", magic, info);
+
+    parse_loader_info(info);
 
     halt_cpu_forever();
 }
